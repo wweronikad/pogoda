67d85b18a3278dcaaa246f12b2bf8d5f6b762406
Weronika Dziolak <weronikadz1405@gmail.com>
2024-11-25 12:21:54 +0100

    last

--- src/components/Headers/Footer.jsx
+++ src/components/Headers/Footer.jsx
@@ -4,13 +4,13 @@ import './Footer.css';
 const apiSources = [
   { name: "zanieczyszczenia powietrza GIOŚ", url: "https://powietrze.gios.gov.pl/pjp/content/api" },
   { name: "dane IMGW synoptyczne, hydrologiczne", url: "https://danepubliczne.imgw.pl/pl/apiinfo" },
-  { name: "dane przemieszczania słońca", url: "https://sunrise-sunset.org/api" }
+  { name: "dane przemieszczania słońca Sunrise Sunset", url: "https://sunrise-sunset.org/api" },
+  { name: "dane lokalizacyjne Nominatim", url: "https://nominatim.openstreetmap.org/ui/search.html" }
 ];
 
 function Footer() {
   return (
     <footer className="footer">
-      <p>Praca inżynierska Weronika Dziołak 2024/2025.</p>
       <p>
         Źródła API:{" "}
         {apiSources.map((source, index) => (
